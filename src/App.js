3aec34041ff72c75a12210be8ad57709299428b3
Wendy-bear <w6363980@gmail.com>
2021-12-06 20:15:06 +0300

    +

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import { TwitterPicker } from 'react-color';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Pie Chart</h1>
+      <TwitterPicker></TwitterPicker>
     </div>
   );
 }
